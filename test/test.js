53635412abda12c227bd79dc6bf7c6f213bd4584
unknown <epop.bill@gmail.com>
2019-01-20 19:21:07 -0500

    text change

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const request = require('../post');
 const calc = require('../calculate');
 
    describe('calc()', function(){
-	    it('should find the remainder of two numbers', function(done){
+	    it('should add, subtract, divide, multiply, or find remainder of two numbers', function(done){
 
 			api.get("https://interview.adpeai.com/api/v1/get-task").then(function(res){
 
